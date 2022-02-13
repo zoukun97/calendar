8a5daddc5617d88cbaddce5066aa60879b9a59a4
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2022-02-13 19:35:17 +0900

    conditions#indexのテストを追加

--- spec/requests/conditions_spec.rb
+++ spec/requests/conditions_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Conditions', type: :request do
+
+  let!(:user) { create(:user) }
+  let!(:conditions) { create_list(:condition, 3, user: user) }
+  
   describe 'GET /conditions' do
     it '200ステータスが返ってくる' do
       get conditions_path

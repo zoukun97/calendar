1ea2ec3adbd8c1eddf29b5fc46aaae7e001406b5
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2022-02-26 19:43:32 +0900

    year model  削除

---
+++ db/migrate/20220226103816_drop_to_year.rb
@@ -0,0 +1,3 @@
+class DropToYear < ActiveRecord::Migration[6.1]
+  drop_table :years
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_101333) do
+ActiveRecord::Schema.define(version: 2022_02_26_103816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,11 +49,6 @@ ActiveRecord::Schema.define(version: 2022_02_26_101333) do
     t.integer "point", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "years", primary_key: "day", id: :date, force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
